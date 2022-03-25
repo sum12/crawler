0bcb43be62972be713fd12b33996825081626849
sum12 <sumitjami@gmail.com>
2022-03-25 16:20:31 +0100

    give player points for waiting

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -4,6 +4,7 @@ use crate::prelude::*;
 #[read_component(Point)]
 #[read_component(Player)]
 #[read_component(Enemy)]
+#[write_component(Health)]
 pub fn player_input(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
@@ -21,18 +22,20 @@ pub fn player_input(
         };
 
         let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
+        let mut did_something = false;
 
+        let (player_entity, destination) = player
+            .iter(ecs)
+            .find_map(|(entity, pos)| Some((*entity, *pos + delta)))
+            .unwrap();
         if delta.x != 0 || delta.y != 0 {
             let mut hit_something = false;
-            let (player_entity, destination) = player
-                .iter(ecs)
-                .find_map(|(entity, pos)| Some((*entity, *pos + delta)))
-                .unwrap();
             enemies
                 .iter(ecs)
                 .filter(|(_, pos)| **pos == destination)
                 .for_each(|(enemy, _)| {
                     hit_something = true;
+                    did_something = true;
                     commands.push((
                         (),
                         WantsToAttack {
@@ -41,7 +44,9 @@ pub fn player_input(
                         },
                     ));
                 });
+
             if !hit_something {
+                did_something = true;
                 commands.push((
                     (),
                     WantsToMove {
@@ -49,6 +54,15 @@ pub fn player_input(
                         destination,
                     },
                 ));
+            }
+        }
+        if !did_something {
+            if let Ok(mut health) = ecs
+                .entry_mut(player_entity)
+                .unwrap()
+                .get_component_mut::<Health>()
+            {
+                health.current = i32::min(health.max, health.current + 1);
             }
         }
         *turn_state = TurnState::PlayerTurn;
